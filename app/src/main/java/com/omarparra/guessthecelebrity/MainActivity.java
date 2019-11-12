f5635cc568a8b5a9162893bb11a8e675131188e1
Omar Parra <omarfdez92@gmail.com>
2019-11-11 23:29:26 -0600

    Initial commit

---
+++ app/src/main/java/com/omarparra/guessthecelebrity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.omarparra.guessthecelebrity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //resource http://www.posh24.se/kandisar
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
